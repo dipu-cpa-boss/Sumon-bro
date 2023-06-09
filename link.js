3f34c140a708b3916fa0702135d289a5788e645a
dipu islam <121944271+dipu-cpa-boss@users.noreply.github.com>
2023-06-09 23:20:07 +0600

    Update link.js

--- link.js
+++ link.js
@@ -1,1 +1,1 @@
-const offerLink = "https://wabxeq.datingllfe.net/?utm_source=da57dc555e50572d&s1=193358&s2=1871727&s3=Rj&s5=Rj&j5=1&j6=1";
+const offerLink = "https://wabxeq.datlngllfe.net/?utm_source=da57dc555e50572d&s1=193358&s2=1872874&s3=Rj&j1=1";
